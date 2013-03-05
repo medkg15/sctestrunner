53681aee81eaed2a97f64e5f5743231d8868c97f
Jorge Lusar <jorgitto@gmail.com>
2013-03-05 16:37:48 +0000

    Adding test examples and package

---
+++ src/TestExamples/TestCatOne.cs
@@ -0,0 +1,54 @@
+﻿namespace NUnitContrib.Web.TestExamples
+{
+    using System;
+    using System.Threading;
+    using System.Web;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestCatOne
+    {
+        [Test(Description = "Description One"), Category("Cat One")]
+        public void ShortTest()
+        {
+            Console.WriteLine("Hello");
+            Assert.IsTrue(true);
+            Console.WriteLine("Bye");
+        }
+
+        [Test, Category("Cat One")]
+        public void LongTest()
+        {
+            Console.WriteLine("Hola");
+            Thread.Sleep(2000);
+            Assert.IsTrue(true);
+        }
+
+        [Test, Category("Cat One")]
+        public void HttpContextNotNull()
+        {
+            Assert.NotNull(HttpContext.Current);
+        }
+
+        [Test]
+        public void HttpSessionNotNull()
+        {
+            HttpContext.Current.Session["IShouldNotBeNull"] = true;
+            HttpContext.Current.Session["IShouldHaveAName"] = "Your name";
+            Assert.IsTrue((bool)HttpContext.Current.Session["IShouldNotBeNull"]);
+            Assert.AreEqual(HttpContext.Current.Session["IShouldHaveAName"], "Your name");
+        }
+
+        [Test]
+        public void ThisTestShouldFail()
+        {
+            Assert.AreEqual(2, 3);
+        }
+
+        [Test, Ignore("The reason to ignore this test")]
+        public void ThisTestShouldBeIgnored()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
---
+++ src/TestExamples/TestCatTwo.cs
@@ -0,0 +1,41 @@
+﻿namespace NUnitContrib.Web.TestExamples
+{
+    using System;
+    using System.Threading;
+    using NUnit.Framework;
+
+    [TestFixture, Category("Cat Two")]
+    public class TestCatTwo
+    {
+        [Test, Category("Cat Two")]
+        public void ShortTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test, Category("Cat Three")]
+        public void ShouldBeCatTwoAndThree()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void ShouldThrowException()
+        {
+            throw new Exception("This exception is a test");
+        }
+
+        [Test, ExpectedException]
+        public void ShouldCatchException()
+        {
+            throw new Exception("This exception is another test");
+        }
+
+        [Test, Category("Cat Two")]
+        public void LongTest()
+        {
+            Thread.Sleep(3000);
+            Assert.IsTrue(true);
+        }
+    }
+}
